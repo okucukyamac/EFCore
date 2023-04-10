fdab40506a30568f5579bc3187977e9a19d26647
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-04-11 01:00:28 +0300

    AppDbContext

---
+++ DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst.DAL
+{
+    public class AppDbContext:DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext()
+        {
+
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+    }
+}
--- DatabaseFirst/Program.cs
+++ DatabaseFirst/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+using DatabaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+DbContextInitializer.Build();
+
+using (var _context = new AppDbContext(DbContextInitializer.OptionsBuilder.Options))
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(a =>
+    {
+        Console.WriteLine(a.Name);
+    });
+    
+}
+
 Console.WriteLine("Hello, World!");
