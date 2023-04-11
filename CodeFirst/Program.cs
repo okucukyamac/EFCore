322cd17c3b959a42727210677602fbb89e1f16e0
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-04-11 23:45:09 +0300

    dbfirst new project

---
+++ CodeFirst/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using CodeFirst;
+using CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
+
+using (var _context = new AppDbContext)
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(a =>
+    {
+        Console.WriteLine(a.Name);
+    });
+}
+
+Console.WriteLine("Hello, World!");
