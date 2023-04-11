9443802fe0197c424e12bd7aa34a1fff55c366d5
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-04-12 00:35:18 +0300

    migration komutları

--- CodeFirst/Migrations/AppDbContextModelSnapshot.cs
+++ CodeFirst/Migrations/AppDbContextModelSnapshot.cs
@@ -44,7 +44,7 @@ namespace CodeFirst.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Products");
+                    b.ToTable("Products", (string)null);
                 });
 #pragma warning restore 612, 618
         }
