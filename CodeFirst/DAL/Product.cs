5aa37c1ed0fe7cd8319f37992d63ff2a0e7c5e87
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-04-11 23:58:00 +0300

    cmt

--- CodeFirst/DAL/Product.cs
+++ CodeFirst/DAL/Product.cs
@@ -13,5 +13,6 @@ namespace CodeFirst.DAL
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public int Barcode { get; set; }
+        public string Description { get; set; }
     }
 }
